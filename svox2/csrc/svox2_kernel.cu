a2878418e07a34f4ca8d9c58e69e4d45e81fe7e7
Alex Yu <sxyu@berkeley.edu>
2021-10-15 21:47:44 -0700

    Optimized grid sampling kernels

--- svox2/csrc/svox2_kernel.cu
+++ svox2/csrc/svox2_kernel.cu
@@ -6,88 +6,89 @@
 
 namespace device {
 
-__device__ __inline__ void trilerp(
-        const torch::PackedTensorAccessor64<float, 2, torch::RestrictPtrTraits> data,
-        const torch::PackedTensorAccessor32<int32_t, 3, torch::RestrictPtrTraits> links,
-        float* __restrict__ pos,
-        float* __restrict__ out) {
-    int32_t l[3];
-
-#pragma unroll 3
-    for (int i = 0; i < 3; ++i) {
-        pos[i] = min(max(pos[i], 0.f), links.size(i) - 1.f);
-        l[i] = min((int32_t)pos[i], (int32_t)(links.size(i) - 2));
-        pos[i] -= l[i];
-    }
-    const int32_t* __restrict__ link_ptr = &links[l[0]][l[1]][l[2]];
-    const int offy = links.stride(1), offx = links.stride(0);
-    const int data_dim = data.size(1);
-
-    float buffer[8][49];
-    const float* __restrict__ dptr = data.data();
-    _COPY_8_NEIGHBORS(dptr, link_ptr, offx, offy, buffer, data_dim, 0, data_dim);
-
-    _trilerp_buffered(buffer, out, pos, 0, data_dim);
-}
-
-
-__device__ __inline__ void trilerp_backward(
-        torch::PackedTensorAccessor32<int32_t, 3, torch::RestrictPtrTraits> links,
-        float* __restrict__ pos,
-        const float* __restrict__ grad_out,
-        torch::PackedTensorAccessor64<float, 2, torch::RestrictPtrTraits> grad_data) {
-    int32_t l[3];
-#pragma unroll 3
-    for (int i = 0; i < 3; ++i) {
-        pos[i] = min(max(pos[i], 0.f), links.size(i) - (1.f + 1e-3f));
-        l[i] = min((int32_t)pos[i], (int32_t)(links.size(i) - 2));
-        pos[i] -= l[i];
-    }
-
-    const int data_dim = grad_data.size(1);
-
-    float buffer[8][49];
-    float lerp_wt[8];
-    _init_lerp_weight(lerp_wt, pos);
-
-#pragma unroll
-    for (int j = 0; j < data_dim; ++j) {
-#pragma unroll 8
-        for (int k = 0; k < 8; ++k)
-            buffer[k][j] = lerp_wt[k] * grad_out[j];
-    }
-
-    // TODO: candidate for coalesced atomics, although this function is not critical
-    const int32_t* __restrict__ link_ptr = &links[l[0]][l[1]][l[2]];
-    float* __restrict__ gdptr = grad_data.data();
-    const int offy = links.stride(1), offx = links.stride(0);
-    _UPDATE_8_NEIGHBORS(gdptr, link_ptr, offx, offy, buffer, data_dim);
-}
-
 __global__ void sample_grid_kernel(
         PackedSparseGridSpec grid,
         const torch::PackedTensorAccessor32<float, 2, torch::RestrictPtrTraits> points,
         // Output
         torch::PackedTensorAccessor32<float, 2, torch::RestrictPtrTraits> out) {
-    CUDA_GET_THREAD_ID(tid, points.size(0));
+    CUDA_GET_THREAD_ID(tid, points.size(0) * grid.data.size(1));
+    const int idx = tid % grid.data.size(1);
+    const int pid = tid / grid.data.size(1);
 
-    float point[3] = {points[tid][0], points[tid][1], points[tid][2]};
+    float point[3] = {points[pid][0], points[pid][1], points[pid][2]};
     transform_coord(point, grid._scaling, grid._offset);
-    // Destroys point
-    trilerp(grid.data, grid.links, point, &out[tid][0]);
+
+    int32_t l[3];
+#pragma unroll 3
+    for (int i = 0; i < 3; ++i) {
+        point[i] = fminf(fmaxf(point[i], 0.f), grid.links.size(i) - 1.f);
+        l[i] = min((int32_t)point[i], (int32_t)(grid.links.size(i) - 2));
+        point[i] -= l[i];
+    }
+
+    const int32_t* __restrict__ link_ptr = &grid.links[l[0]][l[1]][l[2]];
+
+    const int offy = grid.links.stride(1), offx = grid.links.stride(0);
+
+#define MAYBE_READ_LINK(u) ((link_ptr[u] >= 0) ? grid.data[link_ptr[u]][idx] : 0.f)
+
+    const float ix0y0 = lerp(MAYBE_READ_LINK(0), MAYBE_READ_LINK(1), point[2]);
+    const float ix0y1 = lerp(MAYBE_READ_LINK(offy), MAYBE_READ_LINK(offy + 1), point[2]);
+    const float ix0 = lerp(ix0y0, ix0y1, point[1]);
+    const float ix1y0 = lerp(MAYBE_READ_LINK(offx), MAYBE_READ_LINK(offx + 1), point[2]);
+    const float ix1y1 = lerp(MAYBE_READ_LINK(offy + offx),
+                             MAYBE_READ_LINK(offy + offx + 1), point[2]);
+    const float ix1 = lerp(ix1y0, ix1y1, point[1]);
+    out[pid][idx] = lerp(ix0, ix1, point[0]);
 }
 
 __global__ void sample_grid_backward_kernel(
         PackedSparseGridSpec grid,
-        torch::PackedTensorAccessor32<float, 2, torch::RestrictPtrTraits> points,
-        torch::PackedTensorAccessor32<float, 2, torch::RestrictPtrTraits> grad_out,
+        const torch::PackedTensorAccessor32<float, 2, torch::RestrictPtrTraits> points,
+        const torch::PackedTensorAccessor32<float, 2, torch::RestrictPtrTraits> grad_out,
         // Output
         torch::PackedTensorAccessor64<float, 2, torch::RestrictPtrTraits> grad_data) {
-    CUDA_GET_THREAD_ID(tid, points.size(0));
-    float point[3] = {points[tid][0], points[tid][1], points[tid][2]};
+    CUDA_GET_THREAD_ID(tid, points.size(0) * grid.data.size(1));
+    const int idx = tid % grid.data.size(1);
+    const int pid = tid / grid.data.size(1);
+
+    float point[3] = {points[pid][0], points[pid][1], points[pid][2]};
     transform_coord(point, grid._scaling, grid._offset);
-    // Destroys point
-    trilerp_backward(grid.links, point, &grad_out[tid][0], grad_data);
+
+    int32_t l[3];
+#pragma unroll 3
+    for (int i = 0; i < 3; ++i) {
+        point[i] = fminf(fmaxf(point[i], 0.f), grid.links.size(i) - 1.f);
+        l[i] = min((int32_t)point[i], (int32_t)(grid.links.size(i) - 2));
+        point[i] -= l[i];
+    }
+
+    const int32_t* __restrict__ link_ptr = &grid.links[l[0]][l[1]][l[2]];
+
+    const int offy = grid.links.stride(1), offx = grid.links.stride(0);
+    const float go = grad_out[pid][idx];
+
+    const float xb = point[0], yb = point[1], zb = point[2];
+    const float xa = 1.f - point[0], ya = 1.f - point[1], za = 1.f - point[2];
+
+#define MAYBE_ADD_GRAD_LINK_PTR(u, content) if (link_ptr[u] >= 0)  \
+            atomicAdd(&grad_data[link_ptr[u]][idx], content) 
+
+    const float xago = xa * go;
+    float tmp = ya * xago;
+    MAYBE_ADD_GRAD_LINK_PTR(0, tmp * za);
+    MAYBE_ADD_GRAD_LINK_PTR(1, tmp * zb);
+    tmp = yb * xago;
+    MAYBE_ADD_GRAD_LINK_PTR(offy, tmp * za);
+    MAYBE_ADD_GRAD_LINK_PTR(offy + 1, tmp * zb);
+
+    const float xbgo = xb * go;
+    tmp = ya * xbgo;
+    MAYBE_ADD_GRAD_LINK_PTR(offx, tmp * za);
+    MAYBE_ADD_GRAD_LINK_PTR(offx + 1, tmp * zb);
+    tmp = yb * xbgo;
+    MAYBE_ADD_GRAD_LINK_PTR(offx + offy, tmp * za);
+    MAYBE_ADD_GRAD_LINK_PTR(offx + offy + 1, tmp * zb);
 }
 }  // namespace device
 
@@ -97,10 +98,11 @@ torch::Tensor sample_grid(SparseGridSpec& grid, torch::Tensor points) {
     grid.check();
     CHECK_INPUT(points);
     TORCH_CHECK(points.ndimension() == 2);
-    const auto Q = points.size(0);
+    const auto Q = points.size(0) * grid.data.size(1);
     const int cuda_n_threads = std::min<int>(Q, CUDA_MAX_THREADS);
     const int blocks = CUDA_N_BLOCKS_NEEDED(Q, cuda_n_threads);
-    torch::Tensor result = torch::empty({Q, grid.data.size(1)}, points.options());
+    torch::Tensor result = torch::empty({points.size(0),
+                        grid.data.size(1)}, points.options());
 
     device::sample_grid_kernel<<<blocks, cuda_n_threads>>>(
             grid,
@@ -121,12 +123,12 @@ torch::Tensor sample_grid_backward(
     CHECK_INPUT(grad_out);
     TORCH_CHECK(points.ndimension() == 2);
     TORCH_CHECK(grad_out.ndimension() == 2);
-    const auto Q = points.size(0);
+    const auto Q = points.size(0) * grid.data.size(1);
 
     const int cuda_n_threads = std::min<int>(Q, CUDA_MAX_THREADS);
     const int blocks = CUDA_N_BLOCKS_NEEDED(Q, cuda_n_threads);
 
-    torch::Tensor grad_data = torch::empty({grid.data.requires_grad() ? grid.data.size(0) : 0,
+    torch::Tensor grad_data = torch::zeros({grid.data.requires_grad() ? grid.data.size(0) : 0,
                                              grid.data.size(1)},
                                              points.options());
 
--- test/test_sample.py
+++ test/test_sample.py
@@ -11,11 +11,11 @@ grid.data.data[..., 1:].normal_(0.0, 1.0)
 grid.data.data[..., 0] = 1.0
 #  grid = torch.rand((2, 2, 2, 4), device=device, dtype=torch.float32)
 
-N_POINTS = 400000
+N_POINTS = 5000 * 1024
 points = torch.rand(N_POINTS, 3, device=device) * 2 - 1
 #  points = torch.tensor([[0.49, 0.49, 0.49], [0.9985, 0.4830, 0.4655]], device=device)
 #  points.clamp_(-0.999, 0.999)
- 
+
 sampc = grid.sample(points)
 sampt = grid.sample(points, use_kernel=False)
 
@@ -27,7 +27,7 @@ with Timing("ours"):
 s = sampc.sum()
 with Timing("our_back"):
     s.backward()
-g1 = grid.data.grad.clone()
+go = grid.data.grad.clone()
 grid.data.grad = None
 
 with Timing("torch"):
@@ -35,9 +35,9 @@ with Timing("torch"):
 s = sampt.sum()
 with Timing("torch_back"):
     s.backward()
-g2 = grid.data.grad.clone()
+gt = grid.data.grad.clone()
 
 #  print('c\n', sampc)
 #  print('t\n', sampt)
 print('err\n', torch.abs(sampt-sampc).max())
-print('err_grad\n', torch.abs(g1-g2).max())
+print('err_grad\n', torch.abs(go-gt).max())
